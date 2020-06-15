12292e520c4b5c10784c8dc684a5cdcf52fa6ac2
Pedro Luis De Leon <breidydl@gmail.com>
2020-06-15 13:07:47 -0400

    Se busco la decena inmediata

--- index.js
+++ index.js
@@ -13,37 +13,27 @@
 // var diezPrimeros = sinGuiones.slice(0, 10);
 
 var cedula = [4,0,2,1,0,3,5,0,9,2]
-var porDos = ([cedula[1], cedula[3], cedula[5], cedula[7], cedula[9]]);
 var porUno = ([cedula[0], cedula[2], cedula[4], cedula[6], cedula[8]]);
-
-// var posiciones = [1,2,1,2,1,2,1,2,1,2];
-// 4,2,0,5,9
-// 0,1,3,0,2
+var porDos = ([cedula[1], cedula[3], cedula[5], cedula[7], cedula[9]]);
 
 let xDos = porDos.map((val) => {
     return val * 2;
 }).join("").split("");
 
-union = xDos.concat(porUno);
-// let parseado = parseInt(union)
+let union = xDos.concat(porUno);
 
-
-var parsed = union.map((x) => {
+let parsed = union.map((x) => {
     return parseInt(x);
 })
 
 let sumar = parsed.reduce((a, b) => {
-    return a + b;
+    return ((a + b));
 })
 
-console.log(sumar)
+// Buscar decena inmediata
+let decena = (Math.ceil(sumar / 10) * 10);
 
-// const sumar = (elem) => {
-//     let suma = 0;
-//     console.log(suma += elem);
-// }
 
-// sumar(parsed);
 
 
 
