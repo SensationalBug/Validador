19045603f20caa96b0b4e020de9a8b4e1d9fc1a4
Pedro Luis De Leon <breidydl@gmail.com>
2020-06-15 11:32:50 -0400

    Ya se le aplico una funcion para realizar la multiplicacion correspondiente

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 // Se introduce la cedula con guiones o sin ellos XD
-var cedula = prompt('Ingresa el numero de cédula.');
+// var cedula = prompt('Ingresa el numero de cédula.');
 
 
 // En caso de que la cedula tenga guiones se borran (siempre contienen 2 guiones)
-var guion1 = cedula.replace("-", "");
-var guion2 = guion1.replace("-", "");
+// var guiones = cedula.replace("-", "").replace("-", "");
 // 402-1035092-8
 
 
 // En esta parte se separan los numeros para poner el ultimo en una varible y los 10 primeros en otra
-var sinGuiones = guion2.split("");
-var ultNum = sinGuiones[10];
-var diezPrimeros = sinGuiones.slice(0, 10);
+// var sinGuiones = guiones.split("");
+// var ultNum = sinGuiones[10];
+// var diezPrimeros = sinGuiones.slice(0, 10);
+
+var cedula = [4,0,2,1,0,3,5,0,9,2]
+var porDos = [cedula[0], cedula[2], cedula[4], cedula[6], cedula[8]];
+var porUno = [cedula[1], cedula[3], cedula[5], cedula[7], cedula[9]];
+
+// var posiciones = [1,2,1,2,1,2,1,2,1,2];
+// 4,2,0,5,9
+// 0,1,3,0,2
+
+let dos = porDos.map((val) => {
+    return val * 2;
+})
+
+    
+
+
+
